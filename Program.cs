a03575e38386078cf051f6fed7fb935fff28535d
emisaez <72699615+emisaez@users.noreply.github.com>
2021-05-05 13:17:58 -0400

    Create Program.cs
    Contains the main method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SliceMethod 
+{ 
+    public class Program
+    {
+        public static void Main(String[] arguments)
+        {
+            string simpleText = "¡Hello, World from CSharp!";
+            
+            Console.ReadKey();
+        }
+    }
+}
