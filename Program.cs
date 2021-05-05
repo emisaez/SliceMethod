36494ae9fdee073b643cbeef1fed48f4351b5e04
emisaez <72699615+emisaez@users.noreply.github.com>
2021-05-05 13:44:02 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace SliceMethod
         {
             string simpleText = "¡Hello, World from CSharp!";
             
+		    var r = simpleText.Slice(-4, -1); // arp
+		    var r2 = simpleText.Slice(-4); // arp!
+		    var r3 = simpleText.Slice(0); // ¡Hello, World from CSharp!
+		    var r4 = simpleText.Slice(-20); // , World from CSharp!
+            
+            // print
+            Console.WriteLine(r4);
+            
             Console.ReadKey();
         }
     }
